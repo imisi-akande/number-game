2096f76765b6f6e861efd73c89ed16a48425b6e6
Imisioluwa Akande <imisioluwa.akande@andela.com>
2020-06-01 05:42:12 +0100

    docs:update setup file description

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(name='nummer',
       version='0.1',
-      description='Number classification',
+      description='Guess random numbers and prime numbers',
       packages=['nummer'],
       zip_safe=False)
